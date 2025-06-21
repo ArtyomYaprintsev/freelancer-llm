2b5e48a60328f8a94638835c708045c7165ec16e
ArtyomYaprintsev <yapryntsev.a02@mail.ru>
2025-06-21 16:23:21 +0500

    Create `setup` cli tool

---
+++ setup.py
@@ -0,0 +1,23 @@
+import typer
+from pathlib import Path
+from typing import Annotated
+
+from project.database import setup as setup_database
+
+
+def setup(
+    source: Annotated[
+        Path,
+        typer.Option(help="Dataset filename."),
+    ] = (Path(__file__).parent / "freelancer_earnings_bd.csv"),
+    db_url: Annotated[
+        str,
+        typer.Option(help="Database URL."),
+    ] = "sqlite:///./db.sqlite3",
+    debug: Annotated[bool, typer.Option(help="Show debug info.")] = False,
+):
+    setup_database(source=source, db_url=db_url, debug=debug)
+
+
+if __name__ == "__main__":
+    typer.run(setup)
