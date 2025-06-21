eba58530b8fc09de66f95853fd359e23ba49a63d
ArtyomYaprintsev <yapryntsev.a02@mail.ru>
2025-06-21 16:22:16 +0500

    Remove `config` usage for the `get_engine`

--- project/database/engine.py
+++ project/database/engine.py
@@ -1,12 +1,7 @@
 from functools import lru_cache
 from sqlalchemy import create_engine
 
-from project.config import config
-
 
 @lru_cache()
-def get_engine():
-    return create_engine(
-        url=config.DATABASE_URL,
-        echo=config.SQLALCHEMY_ECHO,
-    )
+def get_engine(url: str, echo: bool):
+    return create_engine(url=url, echo=echo)
